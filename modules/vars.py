de92f62b837c88da2ae969eb6c870e17e5e7db75
AirPheonixSaksham <166398246+AirPheonixSaksham@users.noreply.github.com>
2024-05-25 18:21:49 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
-api_id = 20963688
-api_hash = "964a26dc404aa5e12dfe82ea94ae7ae3"
-bot_token = "7061439554:AAHDXBBktV5P4O6sjBLamdX2hhvBN7uPSl0"
+api_id = 28829496
+api_hash = "8094341dbe5bd50090de8b12a01d04b8"
+bot_token = "6715326658:AAHVLu_V2HJKquZG4kAPydJDP6G1lATc8lA"
 # Bot Username = "allinone"
